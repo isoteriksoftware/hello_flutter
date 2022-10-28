eeabec7072217b0f335c0e8ad25cec5839ba3e25
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-10-28 16:18:52 +0100

    added snippets for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,100 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Profile(),
       ),
-      home: const MyHomePage(title: 'My First Flutter App'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          color: Colors.indigo,
+        ),
+        Center(
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 120)),
+              buildAvatar(),
+              const Padding(padding: EdgeInsets.only(top: 3)),
+              createTextWidget('John Doe', size: 40, weight: FontWeight.w700),
+              createTextWidget(
+                'Flutter Developer',
+                color: Colors.indigo,
+                size: 18,
+                weight: FontWeight.w500,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildSocialButton(Icons.facebook_rounded),
+                  buildSocialButton(Icons.whatsapp_rounded),
+                  buildSocialButton(Icons.email_rounded),
+                  buildSocialButton(Icons.reddit_rounded),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Column buildAvatar() {
+    return Column(
+      children: const [
+        FittedBox(
+          fit: BoxFit.fitWidth,
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/person.jpg'),
+            radius: 70,
+          ),
+        )
+      ],
+    );
+  }
+
+  Text createTextWidget(
+    String text, {
+    double size = 20,
+    FontWeight weight = FontWeight.w400,
+    Color color = Colors.black,
+  }) {
+    return Text(
+      text,
+      style: TextStyle(color: color, fontSize: size, fontWeight: weight),
+    );
+  }
+
+  Widget buildSocialButton(IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: IconButton(
+        onPressed: (){},
+        icon: Icon(
+          icon,
+          color: Colors.indigo,
+          size: 40,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
